830abff8deb1d62ddf7ae4a5cd629a4af3f550b2
SkriLL <timurkazah1998@gmail.com>
2023-05-28 12:54:46 +0500

    cinephile header upcomming added

---
+++ src/components/Upcoming/Upcoming.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main__upcoming">
+        <UpcomingItem />
+    </div>
+</template>
+
+<script setup>
+import UpcomingItem from './UpcomingItem.vue';
+
+</script>
+
+<style lang="scss" scoped></style>
